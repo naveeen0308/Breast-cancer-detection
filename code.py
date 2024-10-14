907e32ce5e0f2dbf919b2e4475139d2b5039cb60
naveeen0308 <snaveen8105@gmail.com>
2024-10-14 12:14:40 +0100

    Update code.py

--- code.py
+++ code.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import numpy as np
 
+#You can use any other ML algorithms instead of adaboost
+
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
